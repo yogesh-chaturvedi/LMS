97937002774c8ece4304b324c8a1d90084a8ad66
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-10-01 18:07:58 +0530

    Fix cors

--- Backend/main.js
+++ Backend/main.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(cookieParser())
 app.use(bodyParser.json()) // to parse the requests which comes from the frontend...
 app.use(cors({
-    origin: 'lms-phi-azure.vercel.app',
+    origin: 'https://lms-phi-azure.vercel.app',
     credentials: true
 }))
 
