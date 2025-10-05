eb7fbfef97e291c747c6d32b307b83a453460646
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-10-05 14:38:53 +0530

    Fix cors

--- Backend/main.js
+++ Backend/main.js
@@ -20,13 +20,13 @@ const CourseCreationRoute = require('./routes/CourseCreation')
 const port = process.env.PORT || 3000;
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// 'https://lms-phi-azure.vercel.app'
+// 'https://elearnings-phi-azure.vercel.app'
 // http://localhost:5173 
 
 app.use(cookieParser())
 app.use(bodyParser.json()) // to parse the requests which comes from the frontend...
 app.use(cors({
-    origin: 'https://lms-phi-azure.vercel.app',
+    origin: 'https://elearnings-phi-azure.vercel.app',
     credentials: true
 }))
 
